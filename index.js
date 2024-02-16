2c7a132b64025021e8a14c5e1f5057f30b123987
hadreemustaffa <hadree.mustaffa@gmail.com>
2024-02-16 14:57:38 +0800

    feat: darken header background as user scroll down on mobile

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const navToggle = document.querySelector('#navToggle');
 const navList = document.querySelector('#navList');
+const header = document.querySelector('header');
 
 navToggle.addEventListener('click', () => {
   navList.classList.toggle('opened');
@@ -9,4 +10,22 @@ navToggle.addEventListener('click', () => {
   } else {
     navToggle.setAttribute('aria-expanded', 'false');
   }
+});
+
+function isMobile() {
+  return window.matchMedia('(max-width: 768px)').matches;
+}
+
+function darkenHeaderOnScroll() {
+  let lastScrollPosition = scrollY;
+  if (lastScrollPosition > 50 && isMobile()) {
+    header.style.backgroundColor = 'var(--clr-neutral-darkest)';
+    header.style.padding = '1rem';
+  } else {
+    header.removeAttribute('style');
+  }
+}
+
+document.addEventListener('scroll', () => {
+  darkenHeaderOnScroll();
 });
